e45429a9a093e38413beeeac729a01fe34801ca0
Bruno Rodrigues <brodriguesco@protonmail.com>
2023-11-12 21:45:59 +0100

    Update default.nix

--- default.nix
+++ default.nix
@@ -1,13 +1,13 @@
 let
  pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/976fa3369d722e76f37c77493d99829540d43845.tar.gz") {};
  rpkgs = builtins.attrValues {
-  inherit (pkgs.rPackages) quarto;
+  inherit (pkgs.rPackages) quarto michelRodange;
 };
   tex = (pkgs.texlive.combine {
   inherit (pkgs.texlive) scheme-small amsmath framed fvextra environ fontawesome5 orcidlink pdfcol tcolorbox tikzfill;
 });
  system_packages = builtins.attrValues {
-  inherit (pkgs) R glibcLocalesUtf8 quarto michelRodange;
+  inherit (pkgs) R glibcLocalesUtf8 quarto;
 };
   in
   pkgs.mkShell {
